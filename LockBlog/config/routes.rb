2e1e2c408e3fab92c9045f591213e8fd9fadfe1e
Doola <ahmedabodeif1@Ahmeds-MacBook-Pro.local>
2015-02-16 10:01:12 +0200

    enable users
    You can know create a new user, edit information and delete user. Admin
    concept not yet deployed. No validations available on the repetition of
    users email and no sign in function available.

--- LockBlog/config/routes.rb
+++ LockBlog/config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  resources :users
+
   get 'welcome/index'
    resources :articles
+   resources :users
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
