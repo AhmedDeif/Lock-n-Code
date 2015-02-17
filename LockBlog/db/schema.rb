f2d968aa96c0b09dc1595585bf25a08e8e7c8e1f
omaradelahmed <omar.adelahmed@student.guc.edu.eg>
2015-02-17 03:06:14 +0200

    Category
    add category to article

--- LockBlog/db/schema.rb
+++ LockBlog/db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150216072249) do
+ActiveRecord::Schema.define(version: 20150217005727) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
     t.text     "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "category"
   end
 
   create_table "users", force: :cascade do |t|
