301044ff6ec63ff5035221e2d81823dd763d3d07
kareem <al3ameeeel94@gmail.com>
2015-02-17 04:47:05 +0300

    commenting on a blog
    added the commenting function on the blog with the ability of embedding
    Youtube videos or images , the commenting is not finished as a
    discussion is required to determine the method used for commenting on
    comments.

--- LockBlog/config/routes.rb
+++ LockBlog/config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
   resources :users
-
   get 'welcome/index'
-   resources :articles
-   resources :users
+  resources :articles
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
@@ -11,6 +9,10 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'welcome#index'
 
+  resources :articles do
+  resources :comments
+  end
+  
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
--- LockBlog/db/schema.rb
+++ LockBlog/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150216072249) do
+ActiveRecord::Schema.define(version: 20150216194047) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -19,6 +19,18 @@ ActiveRecord::Schema.define(version: 20150216072249) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  create_table "comments", force: :cascade do |t|
+    t.text     "commented_to"
+    t.string   "commenter"
+    t.text     "body"
+    t.text     "url"
+    t.integer  "article_id"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
+
+  add_index "comments", ["article_id"], name: "index_comments_on_article_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "fisrt_name"
