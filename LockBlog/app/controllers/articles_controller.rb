daaa06a3b686b9dcbb6ff59d28067e02baf2aaca
omaradelahmed <omar.adelahmed@student.guc.edu.eg>
2015-02-17 22:42:45 +0200

    post-done-tested
    only Admin and authorized users can post

--- LockBlog/app/controllers/articles_controller.rb
+++ LockBlog/app/controllers/articles_controller.rb
@@ -11,6 +11,8 @@ class ArticlesController < ApplicationController
         @article = Article.new
     end
     def create
+        @user = User.find(session[:user_id])
+        if(@user.admin == true or @user.authorized == true)
         @article = Article.new(article_params)
         if @article.save!
             puts @article.image
@@ -18,6 +20,10 @@ class ArticlesController < ApplicationController
         else
             render 'new'
         end
+    else
+        redirect_to Article.new
+    end
+
     end
 
 
--- LockBlog/db/schema.rb
+++ LockBlog/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150217174949) do
+ActiveRecord::Schema.define(version: 20150217200849) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 20150217174949) do
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
     t.string   "profilePicture"
+    t.boolean  "authorized"
   end
 
 end
