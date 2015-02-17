9ff8bab10dc37615aa2cdc9f1da76d19595ac8e7
Rania Abdel Monem <raniimenem@gmail.com>
2015-02-17 16:28:07 +0200

    showing recent posts

--- LockBlog/app/models/article.rb
+++ LockBlog/app/models/article.rb
@@ -1,2 +1,6 @@
 class Article < ActiveRecord::Base
+	 default_scope -> { order(created_at: :desc) }
+	#date_select("article", "written_on", selected: Date.today)
+	validates :title, presence: true,
+                    length: { minimum: 5 }
 end
