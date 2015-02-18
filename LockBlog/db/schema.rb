c0b8dbeffe1770d639967f2a234e610247c7521b
elhagin <oahagin@gmail.com>
2015-02-18 20:54:48 +0200

    migrated signature column

--- LockBlog/db/schema.rb
+++ LockBlog/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150217200849) do
+ActiveRecord::Schema.define(version: 20150218141124) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -39,11 +39,11 @@ ActiveRecord::Schema.define(version: 20150217200849) do
     t.string   "last_name"
     t.string   "email"
     t.string   "password"
-    t.boolean  "admin"
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
     t.string   "profilePicture"
     t.boolean  "authorized"
+    t.text     "signature"
   end
 
 end
