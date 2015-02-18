2d4bb688ba7446e691833195e4ca738d787b3cfa
kareem <al3ameeeel94@gmail.com>
2015-02-19 00:36:50 +0300

    Revert "added reply function"
    This reverts commit 21570982bab15e69c6ba0ffbd66ecede9bd5a62d.

--- LockBlog/app/models/article.rb
+++ LockBlog/app/models/article.rb
@@ -1,5 +1,12 @@
 class Article < ActiveRecord::Base
-	has_many :comments
-  validates :title, presence: true,
-                    length: { minimum: 5 }
+    validates :title, presence: true,
+    length: { minimum: 5 }
+    validates :text, presence: true,
+    length: { minimum: 5 }
+
+    has_many :comments
+
+
+    mount_uploader :image, ImageUploader
+
 end
