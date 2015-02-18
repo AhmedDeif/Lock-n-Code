6022620e05765dd0f7ff638380a3f96deafd2b19
elhagin <oahagin@gmail.com>
2015-02-18 12:53:17 +0200

    removed extra tzinfo gem and resolved conflict

--- LockBlog/db/schema.rb
+++ LockBlog/db/schema.rb
@@ -11,11 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 20150217005727) do
-=======
 ActiveRecord::Schema.define(version: 20150217132843) do
->>>>>>> FETCH_HEAD
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
