e79ded6166211bfcde36694b881e2cf7347d5842
elhagin <oahagin@gmail.com>
2015-02-18 23:48:23 +0200

    added signature to signup

--- LockBlog/app/controllers/users_controller.rb
+++ LockBlog/app/controllers/users_controller.rb
@@ -72,6 +72,6 @@ end
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:fisrt_name, :last_name, :email, :password, :profilePicture)
+      params.require(:user).permit(:fisrt_name, :last_name, :email, :password, :profilePicture, :signature)
     end
 end
