1c6fb36d0a0449da5a6b3723d733f804465b571a
Amira ElEwady <amiraelewady@live.com>
2015-02-18 11:13:44 +0200

    edited destroy

--- LockBlog/app/controllers/users_controller.rb
+++ LockBlog/app/controllers/users_controller.rb
@@ -54,11 +54,43 @@ class UsersController < ApplicationController
   # DELETE /users/1
   # DELETE /users/1.json
   def destroy
-    @user.destroy
+    @user = User.find(session[:user_id])
+    @user2 = User.find(params[:id])
+
+    if @user.admin == true
+
+    @user2.destroy
     respond_to do |format|
       format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
       format.json { head :no_content }
     end
+    else
+       respond_to do |format|
+      format.html { redirect_to users_url, notice: 'you are not an admin.' }
+      format.json { head :no_content }
+
+    end
+  end
+
+  if @user = current_user
+
+   #@user ==user.id
+     #User.destroy(user.id)
+     @user.destroy
+    respond_to do |format|
+      format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  else
+     respond_to do |format|
+      format.html { redirect_to users_url, notice: 'you are not an admin.' }
+      format.json { head :no_content }
+
+    end
+  end
+
+
+
   end
 def isAdmin
 @user.find(params[:admin])
