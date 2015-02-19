f091e3d195e66bdbd81837f23b3dfa1da6e5f5cc
khaled-elsheikh <khaledelsheikh994@gmail.com>
2015-02-19 21:10:20 +0200

    removed file selection
    removed the file selection from the page of the new article as the
    admin will be able to add images and videos as URL in the text field of
    the article

--- LockBlog/db/schema.rb
+++ LockBlog/db/schema.rb
@@ -17,14 +17,11 @@ ActiveRecord::Schema.define(version: 20150218141124) do
   create_table "articles", force: :cascade do |t|
     t.string   "title"
     t.text     "text"
-    t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string   "category"
     t.string   "image"
   end
-
-  add_index "articles", ["user_id"], name: "index_articles_on_user_id"
 
   create_table "comments", force: :cascade do |t|
     t.string   "commenter"
