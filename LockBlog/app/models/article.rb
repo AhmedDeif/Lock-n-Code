12c03d47bc0c313d62a7d65b04fb2c4a08cb6fd3
Amira ElEwady <amiraelewady@live.com>
2015-02-19 19:16:58 +0200

    destroy
    discard changes

--- LockBlog/app/models/article.rb
+++ LockBlog/app/models/article.rb
@@ -1,7 +1,15 @@
 class Article < ActiveRecord::Base
-    validates :title, presence: true,
+	validates :title, presence: true,
     length: { minimum: 5 }
     validates :text, presence: true,
     length: { minimum: 5 }
+
     has_many :comments
+
+
+    mount_uploader :image, ImageUploader
+	
+	has_many :comments
+  validates :title, presence: true,
+                    length: { minimum: 5 }
 end
--- LockBlog/db/schema.rb
+++ LockBlog/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150216194047) do
+ActiveRecord::Schema.define(version: 20150217200849) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -19,19 +19,29 @@ ActiveRecord::Schema.define(version: 20150216194047) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string   "category"
+    t.string   "image"
   end
 
   create_table "comments", force: :cascade do |t|
-    t.text     "commented_to"
     t.string   "commenter"
     t.text     "body"
     t.text     "url"
     t.integer  "article_id"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_index "comments", ["article_id"], name: "index_comments_on_article_id"
+
+  create_table "replies", force: :cascade do |t|
+    t.string   "commenter"
+    t.text     "body"
+    t.integer  "comment_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "replies", ["comment_id"], name: "index_replies_on_comment_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "fisrt_name"
@@ -39,8 +49,10 @@ ActiveRecord::Schema.define(version: 20150216194047) do
     t.string   "email"
     t.string   "password"
     t.boolean  "admin"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
+    t.string   "profilePicture"
+    t.boolean  "authorized"
   end
 
 end
