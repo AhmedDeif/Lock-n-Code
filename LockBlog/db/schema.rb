9469c3ab752a00a3f213d6c242bf35331074d933
Amira ElEwady <amiraelewady@live.com>
2015-02-19 21:55:00 +0200

    no change
    no change

--- LockBlog/db/schema.rb
+++ LockBlog/db/schema.rb
@@ -11,22 +11,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20150218141124) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
     t.text     "text"
+    t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string   "category"
     t.string   "image"
   end
 
+  add_index "articles", ["user_id"], name: "index_articles_on_user_id"
+
   create_table "comments", force: :cascade do |t|
     t.string   "commenter"
     t.text     "body"
-    t.text     "url"
     t.integer  "article_id"
     t.integer  "user_id"
     t.datetime "created_at", null: false
@@ -58,7 +59,6 @@ ActiveRecord::Schema.define(version: 20150218141124) do
     t.datetime "updated_at",     null: false
     t.string   "profilePicture"
     t.boolean  "authorized"
-
     t.text     "signature"
   end
 
