885fa00c4c32c5b1b953aed959817b410950c217
kareem <al3ameeeel94@gmail.com>
2015-02-22 23:53:49 +0300

    login/signup fixed reference to relative directory
    removed session[:user_id] in users controller

--- LockBlog/app/controllers/users_controller.rb
+++ LockBlog/app/controllers/users_controller.rb
@@ -81,9 +81,7 @@ end
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_user
-      if(session[:user_id])
         @user = User.find(params[:id])
-      end
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
